005f56cc467f11b8aad2cfd6bab905416bd37751
Shalitha Senarathne <shalitha89@gmail.com>
2019-09-11 00:02:15 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+
+const app = express();
+
+
+app.listen(4000, () => {
+    console.log('App is running on port 4000');
+});
