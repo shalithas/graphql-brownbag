d22d6a582282025bf7dec452d4ff774a87c8952e
Shalitha Senarathne <shalitha89@gmail.com>
2019-09-12 00:38:09 +0800

    serveral changes done to starter pack

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const path = require('path');
+const schema = require('./schema/schema');
 
 const app = express();
 
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql: true
+}));
+
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
 
 app.listen(4000, () => {
     console.log('App is running on port 4000');
